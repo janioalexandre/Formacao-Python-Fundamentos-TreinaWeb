67d30453620d0ad2a0e0718872a5c19a129e2404
Janio Alexandre Ribeiro <janioalexandre@gmail.com>
2025-02-02 22:51:30 -0300

    Operadores aritméticos

---
+++ 5-operadores-aritmeticos.py
@@ -0,0 +1,14 @@
+idade = 38
+idade2 = 40
+
+soma_idades = idade + idade2
+print(f"A soma das idades é {soma_idades}")
+
+subtracao_idades = idade - idade2
+print(f"A subtração das idades é {subtracao_idades}")
+
+multiplicacao_idades = idade * idade2
+print(f"A multiplicação das idades é {multiplicacao_idades}")
+
+media_idades = (idade + idade2) / 2
+print(f"A média das idades é {media_idades}")

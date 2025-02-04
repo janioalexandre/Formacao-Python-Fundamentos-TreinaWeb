4777ee19aacb02f4c24874b721a43b1589161e8d
Janio Alexandre Ribeiro <janioalexandre@gmail.com>
2025-02-04 10:42:40 -0300

    Estrutura de repetição for

---
+++ 7-for.py
@@ -0,0 +1,37 @@
+nome, idade, sobrenome = "Janio", 38, "Alexandre"
+
+for x in range(1, 10):
+    print(x)
+    print(f"Olá, {nome} {sobrenome}, sua idade é {idade}")
+    if x == 2:
+        continue
+    if x == 8:
+        break
+else:
+    print("O loop entrou no else")
+
+idade = 1
+while idade != 0:
+    nome = input("Digite o seu nome: ")
+    idade = int(input("Digite a sua idade: "))
+    sobrenome = input("Digite o seu sobrenome: ")
+
+    if idade == 99:
+        break
+
+    if idade == 98:
+        continue
+        
+    print(f"Olá, {nome} {sobrenome}, sua idade é {idade}")
+
+    media_idades = (idade + idade) / 2
+    print(f"A média das idades é: {media_idades}")
+
+    if idade <= 17:
+        print(f"{nome} é adolescente")
+    elif idade >= 18 and idade <= 50:
+        print(f"{nome} é adulto")
+    else:
+        print(f"{nome} é idoso")
+else:
+    print("O loop entrou no else")
